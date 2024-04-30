5799f731a1a2d7838f05a90cd692a188f00d6ef6
samthatcode <samthatcode@gmail.com>
2024-04-30 14:29:05 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState, lazy, Suspense } from "react";
+import NavBar from "./components/NavBar";
+import { Routes, Route } from "react-router-dom";
+const GetContents = lazy(() => import("./Pages/GetContents"));
+const Contact = lazy(() => import("./Pages/Contact"));
+const Register = lazy(() => import("./Pages/Register"));
+
+import ScrollToTop from "./components/ScrollToTop";
+import ScrollToAnchor from "./hooks/ScrollToView";
+
+function App() {
+  const [navOn, setNavOn] = useState(true);
+
+  return (
+    <>
+      <Suspense
+        fallback={
+          <div className="laoderCont">
+            <div className="loader"></div>
+          </div>
+        }
+      >
+        <ScrollToTop />
+        <ScrollToAnchor />
+        <NavBar navOn={navOn} />
+        <Routes>
+          <Route index element={<GetContents setNavon={setNavOn} />} />
+          <Route path="/contact" element={<Contact setNavon={setNavOn} />} />
+          <Route path="/register" element={<Register setNavon={setNavOn} />} />
+        </Routes>
+      </Suspense>
+    </>
+  );
+}
+
+export default App;
